1b2db2e161dd4e3db887b461ccfd93ed1dd7ed6b
jadeauradc <161012608+jadeauradc@users.noreply.github.com>
2024-02-27 08:58:41 +0800

    Update and rename LaboratoryPart2Continuation.m to LaboratoryPart1and2.m

---
+++ LaboratoryPart1and2.m
@@ -0,0 +1,20 @@
+% Clear
+clear
+clc
+close all
+
+a = factorial(4)
+
+syms t s
+%% Laplace Transform
+
+f3 = (t + 1)*(t + 2)
+F3 = laplace(f3)
+pretty(F3)
+
+
+%% Inverse Laplace
+syms t s
+F4 = (8-3*s+s^2)/s^3
+f4 = ilaplace(F4)
+pretty(f4)
